6b2a145035fe5318793f8b635e7e47d824483b31
Nicole Zhang <zhangnicole555@gmail.com>
2024-11-04 14:12:42 -0800

    add eda functions

--- run.py
+++ run.py
@@ -3,8 +3,16 @@
 import sys
 import json
 
-from src.etl import merge_gut_subject, get_genus_and_covariates
-from src.graph import make_corr_plot, run_ges
+# necessary imports for functions
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import statsmodels.api as sm
+import pylab as py
+import itertools
+
+from src.etl import merge_gut_subject, get_bacteria_and_covariates
+from src.graph import make_corr_plot, run_ges, run_fci
 
 
 def main(targets):
@@ -15,11 +23,13 @@ def main(targets):
 
     if "eda" in targets:
         # make corr plots, scatter plots, qq plots
-        
+
+    
     if "graph" in targets:
         data = pd.read_csv("data/clean.csv")
         make_corr_plot(data, "plots/corr_plot")
         run_ges(data, "graphs/ges_causal_graph")
+        run_fci(data, "graphs/fci_causal_graph")
         
     
 
